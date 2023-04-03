ea80a73b619cba3918fbe1fbedda067bf5264b7b
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-03 15:01:01 -0500

    my first hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import study from './img/study.jpg';
 import './App.css';
 
+
+const Title = (props) => {
+  return <h1> <i>{props.name}'s </i>Study Cards</h1>
+}
+
 function App() {
   const name= 'Shiloh'
   return (
     <div className="App">
       <header className="App-header">
       <img src={study} className="study" alt="logo" />
-
+<Title name='Shiloh'/>
         <h1>
           {name}'s Study Cards
         </h1>
