9a2854bf5098d151f00685b23366552cde18f153
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-03 11:19:12 -0500

    added study image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import study from './img/study.jpg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
+      <img src={study} className="study" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Study Cards
         </p>
         <a
           className="App-link"
