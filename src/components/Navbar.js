1a386ab6706c0022e896c321da3a6a032ba19aab
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 17:49:08 -0500

    updated nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,8 @@ const Navbar = () => {
             <h1>Study Cards</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Card</a>
+                <a href="/create" 
+                >New Card</a>
             </div>
         </nav>
 
