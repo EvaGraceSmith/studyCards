a76bb184cedeba93707fb3ee8e133bcb894ae550
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 17:23:44 -0500

    added home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Title from './components/Title';
 import Button from './components/Button';
 import Cards from './components/Cards';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <header className="App-header">
       <img src={study} className="study" alt="logo" />
 <Title name='Shiloh'/>
