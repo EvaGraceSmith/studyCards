162e0f67d745523770d390d1f067479f7e781d51
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 18:14:27 -0500

    updated button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,16 @@ const Button = () => {
 
     const handleClick = () => {
         console.log('Hello, students!');
+    }
 
         const handleClickAgain = (name) => {
             console.log('Hello, ' + name);
         }
-    }
+    
     return ( 
         <div className='button'>
-                <button onClick={handleClick}>Create new study cards</button>
-        <button onClick={()=>{
-            console.log("Hello")
-        }}>Click me again</button>
+        <button onClick={handleClick}>Create new study cards</button>
+        <button onClick={()=>{handleClickAgain('Shiloh')}}>Click me again</button>
         
         </div> 
 
