fe65aa962796e1fb2c158594a116c94e37cc7bd7
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 17:20:47 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Title from './components/Title';
 import Button from './components/Button';
 import Cards from './components/Cards';
+import Navbar from './components/Navbar';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   const name= 'Shiloh'
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
       <img src={study} className="study" alt="logo" />
 <Title name='Shiloh'/>
