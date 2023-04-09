05af4e1ebcf1a5342ebfea17fa9c072ee1a8da77
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 20:20:41 -0500

    added useState

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,21 @@ import { useState } from 'react';
 
 const Button = () => {
 
+    const [name, setName]= useState('Shiloh');
+
     const handleClick = () => {
-        console.log('Hello, students!', );
+        setName('Shiloh is creating new study cards');
+  
     }
 
 
     
     return ( 
         <div className='button'>
+                    <p>{ name }</p>
         <button onClick={handleClick}>Create new study cards</button>
        
-        
+
         </div> 
 
     );
