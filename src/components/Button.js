1971f4e37de5b2420491c400bc51d91656040ffb
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-08 20:07:40 -0500

    removed button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,17 @@
+import { useState } from 'react';
+
 const Button = () => {
 
-    const handleClick = (e) => {
-        console.log('Hello, students!', e);
+    const handleClick = () => {
+        console.log('Hello, students!', );
     }
 
-        const handleClickAgain = (name) => {
-            console.log('Hello, ' + name);
-        }
+
     
     return ( 
         <div className='button'>
         <button onClick={handleClick}>Create new study cards</button>
-        <button onClick={()=>handleClickAgain('Shiloh')}>Click me again</button>
+       
         
         </div> 
 
