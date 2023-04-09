d6f4ce4576b6cb39e60546365e8985672d4e4f53
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-09 16:32:18 -0500

    added more questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,12 @@ const Home = () => {
 {subject:'history', question: "What state produces the most wine?", answer: 'California', id: 2},
 {subject:'history', question: "What is the capital of the United States?", answer: 'Washington, D.C.', id: 3},
 {subject: 'history', question: 'Who won the 2016 presidential election?', answer: 'Donald Trump', id: 4},
+{subject: 'history', question: 'What state is the Grand Canyon in?', answer: 'Arizona', id: 5},
+{subject: 'history', question: 'When did the United States declare independence from Great Britain?', answer: 'July 4, 1776', id: 6},
+{subject: 'history', question: 'Why did the United States declare independence from Great Britain?', answer: 'Because of unfair taxes', id: 7},
+{subject: 'history', question: 'Who wrote the Declaration of Independence?', answer: 'Thomas Jefferson', id: 8},
+{subject: 'history', question: 'Which president was assassinated in 1963?', answer: 'John F. Kennedy', id: 9},
+{subject: 'history', question: 'Where did the first Thanksgiving take place?', answer: 'Plymouth, Massachusetts', id: 10},
 
     ]);
     return ( 
