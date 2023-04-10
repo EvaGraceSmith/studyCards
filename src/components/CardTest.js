2ff4fc4936fb6261714ced053f3b4c13e5bbeacb
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-10 18:06:35 -0500

    added err message

--- src/components/CardTest.js
+++ src/components/CardTest.js
@@ -1,49 +1,43 @@
 import './Cards.css';
 //**************These are props**This is function passed in as a prop*********
-const CardTest = ({studyCards, heading, }) => {
+const CardTest = ({ studyCards, heading, }) => {
 
-// this is the same thing, but written differently.
-// const CardTest = (props) => {
+    // this is the same thing, but written differently.
+    // const CardTest = (props) => {
 
-//     const studyCards = props.studyCards
+    //     const studyCards = props.studyCards
 
     return (
         <div className="questions"
->
+        >
             <h1>{heading}</h1>
             <div
-            style={{
-                display: 'grid',
-                gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
-                gap: '1rem',
-                marginBottom: '1rem',
-            }}
-            
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+                    gap: '1rem',
+                    marginBottom: '1rem',
+                }}
+
             >
-            {studyCards.map((studyCard) => (
-                <div className="studyCard-preview" key={studyCard.id}>
-                    <div className="flip-card">
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front">
-                                <div className='question'>
-
-                                    <h2>{studyCard.question}</h2>
+                {studyCards.map((studyCard) => (
+                    <div className="studyCard-preview" key={studyCard.id}>
+                        <div className="flip-card">
+                            <div className="flip-card-inner">
+                                <div className="flip-card-front">
+                                    <div className='question'>
+                                        <h2>{studyCard.question}</h2>
+                                    </div>
+                                </div>
+                                <div className="flip-card-back">
+                                    <div className='textBox'>
+                                        <h2>{studyCard.answer}</h2>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flip-card-back">
-                                <div className='textBox'>
-                                    <h2>{studyCard.answer}</h2>
-                                </div>
-                            </div>
-
                         </div>
-
-                        
                     </div>
-
-
-                </div>
-            ))}
+                ))}
 
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,45 +4,46 @@ import CardTest from './CardTest';
 
 
 const Home = () => {
-// 2. once the data is fetched, store it in a state variable
-    const [studyCards, setStudyCards]= useState(null);
+    // 2. once the data is fetched, store it in a state variable
+    const [studyCards, setStudyCards] = useState(null);
 
     const [isLoading, setIsLoading] = useState(true);
 
 
-//1. fetch the data from the server when the component loads/ first renders
-useEffect(() => {
-fetch('http://localhost:8000/studyCards')
-//this returns a promise
-.then(res => { //this is the response from the server
-    return res.json();//this returns another promise that we can use
-})
-.then((data) => { //this is the data from the server
-    // console.log(data);
-    setStudyCards(data);
-    setIsLoading(false);
+    //1. fetch the data from the server when the component loads/ first renders
+    useEffect(() => {
+        fetch('http://localhost:8000/studyCards')
+            //this returns a promise
+            .then(res => { //this is the response from the server
+                return res.json();//this returns another promise that we can use
+            })
+            .then((data) => { //this is the data from the server
+                setStudyCards(data);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
 
-})
-    },[]);
-
-    return ( 
+    return (
         <div className="home">
 
             {isLoading && <div>Loading...</div>}
 
-{/* The code below returns all the cards. Because I want them sorted by subject, I filtered the results */}
-           {/* <CardTest studyCards={studyCards} heading="History Questions"/> */}
+            {/* The code below returns all the cards. Because I want them sorted by subject, I filtered the results */}
+            {/* <CardTest studyCards={studyCards} heading="History Questions"/> */}
 
-{/* // This is the code that filters the cards by subject */}
-{/* //  in order to access this information once it is in json file, I need to use js.  */}
-{/* this is conditional templating in react */}
-{/* if the information on the left is true, then render the right side.  */}
-{/* 3. once the state has a value, the left side will be true and the right side will be rendered */}
-           {studyCards && <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'history')} heading="History Questions"/>}
-             {studyCards &&  <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'geometry')} heading="Geometry Questions" />}
+            {/* // This is the code that filters the cards by subject */}
+            {/* //  in order to access this information once it is in json file, I need to use js.  */}
+            {/* this is conditional templating in react */}
+            {/* if the information on the left is true, then render the right side.  */}
+            {/* 3. once the state has a value, the left side will be true and the right side will be rendered */}
+            {studyCards && <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'history')} heading="History Questions" />}
+            {studyCards && <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'geometry')} heading="Geometry Questions" />}
 
         </div>
-     );
+    );
 }
- 
+
 export default Home;
