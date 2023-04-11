dea923fa80881c95d760d5f7257650f9bc7bc643
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-11 11:42:41 -0500

    added notes re outlet

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      {/* // Outlet is a placeholder for the content of the current route found in index.js */}
       <Outlet />
     </div>
   );
