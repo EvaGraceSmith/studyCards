7d31e7f11bc9225fab1d75b1e6225013546007d3
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-11 11:30:30 -0500

    updated home and cards, and app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import study from './img/study.jpg';
-import Button from './components/Button';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { Outlet } from "react-router-dom";
@@ -7,18 +5,11 @@ import { Outlet } from "react-router-dom";
 
 
 function App() {
-  const name= 'Shiloh'
+
   return (
     <div className="App">
       <Navbar/>
 
-      <header className="App-header">
-      <img src={study} className="study" alt="logo" />
-        <h1>
-          {name}'s Study Cards
-        </h1>
-        <Button/>
-      </header>
       <Home/>
       <Outlet />
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,20 @@
-
-import CardTest from './CardTest';
-import useFetch from '../useFetch';
+import Button from './Button';
+import study from '../img/study.jpg';
 
 
 const Home = () => {
-    //grab the data, but call it studyCards
-const { data: studyCards, isLoading, error } = useFetch('http://localhost:8000/studyCards');
+    const name= 'Shiloh'
 
     return (
-        <div className="home">
-            {/* this is conditional templating in react */}
-            {error && <div>{error}</div>}
-            {isLoading && <div>Loading...</div>}
 
-            {/* The code below returns all the cards. Because I want them sorted by subject, I filtered the results */}
-            {/* <CardTest studyCards={studyCards} heading="History Questions"/> */}
+        <header className="App-header">
+        <img src={study} className="study" alt="logo" />
+          <h1>
+            {name}'s Study Cards
+          </h1>
+          <Button/>
+        </header>
 
-            {/* // This is the code that filters the cards by subject */}
-            {/* //  in order to access this information once it is in json file, I need to use js.  */}
-            {/* this is conditional templating in react */}
-            {/* if the information on the left is true, then render the right side.  */}
-            {/* 3. once the state has a value, the left side will be true and the right side will be rendered */}
-            {studyCards && <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'history')} heading="History Questions" />}
-            {studyCards && <CardTest studyCards={studyCards.filter((cards) => cards.subject === 'geometry')} heading="Geometry Questions" />}
-
-        </div>
     );
 }
 
