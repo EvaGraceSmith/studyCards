d0071c78d937a0e49df522b85efc790f4d17b445
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-11 12:33:36 -0500

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <h1>Study Cards</h1>
             <div className="links">
                 <NavLink className='link' to='/'>Home</NavLink>
-                <NavLink className='link' to='/createnewstudycards'>Create New Study Cards</NavLink>
+                <NavLink className='link' to='/createnewstudycards'>Create New Cards</NavLink>
                 <NavLink className='link' to='/mystudycards'>My Study Cards</NavLink>
                 {/* <a href="/">Home</a>
                 <a href="/New Card" >New Card</a> */}
