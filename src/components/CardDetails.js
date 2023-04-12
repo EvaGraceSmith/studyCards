a47c82089f62e8ad73c9cbb430fc824ed8ab2558
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-12 15:07:05 -0500

    delete function now working

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,17 +1,23 @@
 import { useParams } from 'react-router-dom';
 import useFetch from '../useFetch.js';
+import { useNavigate } from 'react-router-dom';
 
 
 const CardDetails = () => {
 
     const { id } = useParams();
 const {data: studyCards, isLoading, error} = useFetch('http://localhost:8000/studyCards/' + id);
+const navigate = useNavigate();
 
 const handleDelete = () => {
+
     // you could also use studyCards.id instead of id
-    fetch('http://localhost:8000/studyCards/' + id, {
-        method: 'DELETE'
+    fetch('http://localhost:8000/studyCards/' + studyCards.id, {
+        method: 'DELETE',
     }).then(() => {
+        navigate(`/mystudycards`);
+    })
+}
 
     return ( 
         <div className="card-details">
@@ -22,7 +28,8 @@ const handleDelete = () => {
                     <h2>{ studyCards.subject }</h2>
                     <p>Question: { studyCards.question }</p>
                     <p>Answer: { studyCards.answer }</p>
-                    <button OnClick={handleDelete} >Delete</button>
+                    <button onClick={handleDelete} onSubmit={handleDelete}>Delete</button>
+
                 </article>
             )}
   
