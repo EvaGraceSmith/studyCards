a2cca8b3c2ab615ea9059efb8edf67ccb39efa99
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-12 09:01:28 -0500

    added loading message

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
+import useFetch from '../useFetch.js';
 
 
 const CardDetails = () => {
 
     const { id } = useParams();
+const {data: studyCards, isLoading, error} = useFetch('http://localhost:8000/studyCards/' + id);
+
     return ( 
         <div className="card-details">
+            {isLoading && <div>Loading...</div>}
             <h2>Card Details - { id }</h2>   
         </div>
      );
